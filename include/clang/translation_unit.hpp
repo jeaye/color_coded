b46a06ddffb93f989dd9cddc1b1ff137eeda4caf
jeaye <contact@jeaye.com>
2014-09-28 22:04:10 +0800

    Bit of formatting changes

--- include/clang/translation_unit.hpp
+++ include/clang/translation_unit.hpp
@@ -9,14 +9,16 @@ namespace color_coded
 {
   namespace clang
   {
-    static auto const constexpr args(
-    color_coded::detail::make_array
+    static auto const constexpr args
     (
-      "-std=c++1y", "-stdlib=libc++", "-I/usr/include",
-      "-I/usr/lib/clang/3.5.0/include", "-I.", "-Iinclude",
-      "-Ilib/juble/include", "-Ilib/juble/lib/ruby/include",
-      "-Ilib/juble/lib/ruby/.ext/include/x86_64-linux", "-w"
-    ));
+      color_coded::detail::make_array
+      (
+        "-std=c++1y", "-stdlib=libc++", "-I/usr/include",
+        "-I/usr/lib/clang/3.5.0/include", "-I.", "-Iinclude",
+        "-Ilib/juble/include", "-Ilib/juble/lib/ruby/include",
+        "-Ilib/juble/lib/ruby/.ext/include/x86_64-linux", "-w"
+      )
+    );
 
     using translation_unit_impl = CXTranslationUnit;
 
