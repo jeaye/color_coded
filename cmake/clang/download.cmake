e3898524885a7f006b3e094c2eab7eb0ea949c54
jeaye <contact@jeaye.com>
2015-04-07 11:44:18 +0800

    Refactor clang's downloading #25

--- cmake/clang/download.cmake
+++ cmake/clang/download.cmake
@@ -1,17 +1,18 @@
-message("Downloading Clang/LLVM 3.6.0 ...")
+set(CLANG_VERSION "3.6.0")
+message("Downloading Clang/LLVM ${CLANG_VERSION} ...")
 
-set(CLANG_URL "http://llvm.org/releases/3.6.0" )
+set(CLANG_URL "http://llvm.org/releases/${CLANG_VERSION}" )
 if(APPLE)
-  set(CLANG_DIRNAME "clang+llvm-3.6.0-x86_64-apple-darwin")
+  set(CLANG_DIRNAME "clang+llvm-${CLANG_VERSION}-x86_64-apple-darwin")
   set(CLANG_MD5 "103e06006c88d391c9c369a4671e3363")
   set(CLANG_FILENAME "${CLANG_DIRNAME}.tar.xz")
 else()
   if(64_BIT_PLATFORM)
-    set(CLANG_DIRNAME "clang+llvm-3.6.0-x86_64-linux-gnu-ubuntu-14.04")
+    set(CLANG_DIRNAME "clang+llvm-${CLANG_VERSION}-x86_64-linux-gnu-ubuntu-14.04")
     set(CLANG_MD5 "cfb2ebc01573e666770b9c5f72deb04e")
     set(CLANG_FILENAME "${CLANG_DIRNAME}.tar.xz")
   else()
-    message(FATAL_ERROR "No pre-built Clang 3.6.0 binaries for 32 bit Linux; this system is not supported")
+    message(FATAL_ERROR "No pre-built Clang ${CLANG_VERSION} binaries for 32 bit Linux; this system is not supported")
   endif()
 endif()
 
@@ -19,19 +20,18 @@ file(DOWNLOAD
   "${CLANG_URL}/${CLANG_FILENAME}" "./${CLANG_FILENAME}"
   SHOW_PROGRESS EXPECTED_MD5 "${CLANG_MD5}")
 
-message("Extracting Clang/LLVM 3.6.0 ...")
+if(NOT EXISTS ${CLANG_DIRNAME})
+  message("Extracting Clang/LLVM ${CLANG_VERSION} ...")
 
-if(CLANG_FILENAME MATCHES ".+bz2")
-  execute_process(COMMAND tar -xjf ${CLANG_FILENAME})
-elseif(CLANG_FILENAME MATCHES ".+xz")
-  execute_process(COMMAND tar -xJf ${CLANG_FILENAME})
+  if(CLANG_FILENAME MATCHES ".+bz2")
+    execute_process(COMMAND tar -xjf ${CLANG_FILENAME} -C ${CLANG_DIRNAME} --strip-components 1)
+  elseif(CLANG_FILENAME MATCHES ".+xz")
+    execute_process(COMMAND tar -xJf ${CLANG_FILENAME} -C ${CLANG_DIRNAME} --strip-components 1)
+  else()
+    execute_process(COMMAND tar -xzf ${CLANG_FILENAME} -C ${CLANG_DIRNAME} --strip-components 1)
+  endif()
 else()
-  execute_process(COMMAND tar -xzf ${CLANG_FILENAME})
+  message("Clang/LLVM ${CLANG_VERSION} already extracted")
 endif()
 
-# We need to set PATH_TO_LLVM_ROOT. To do that, we first have to find the
-# folder name the archive produced. It isn't the archive base name.
-execute_process(COMMAND
-  find ${CMAKE_CURRENT_BINARY_DIR} -maxdepth 1 -type d -name clang*
-  OUTPUT_VARIABLE LLVM_ROOT_PATH
-  OUTPUT_STRIP_TRAILING_WHITESPACE)
+get_filename_component(LLVM_ROOT_PATH ${CLANG_DIRNAME} ABSOLUTE)
