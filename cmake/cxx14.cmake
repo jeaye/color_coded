4ebb4ee68b4dfaf08ec638d7daa3b77616a1bce4
jeaye <contact@jeaye.com>
2015-04-10 10:31:21 +0800

    Add a check for GCC 4.9+ #25

--- cmake/cxx14.cmake
+++ cmake/cxx14.cmake
@@ -8,3 +8,16 @@ elseif(COMPILER_SUPPORTS_CXX1Y)
 else()
   message(FATAL_ERROR "${CMAKE_CXX_COMPILER} has no C++14 support")
 endif()
+
+if(CMAKE_COMPILER_IS_GNUCXX)
+  exec_program(
+    ${CMAKE_CXX_COMPILER}
+    ARGS --version
+    OUTPUT_VARIABLE _compiler_output)
+  string(REGEX REPLACE ".* ([0-9]\\.[0-9]\\.[0-9]) .*" "\\1" GCC_COMPILER_VERSION ${_compiler_output})
+  message(STATUS "C++ compiler version: ${gcc_compiler_version} [${CMAKE_CXX_COMPILER}]")
+
+  if(${GCC_COMPILER_VERSION} VERSION_LESS "4.9")
+    message(FATAL_ERROR "GCC 4.9 or greater is required; you have GCC ${GCC_COMPILER_VERSION}")
+  endif()
+endif()
