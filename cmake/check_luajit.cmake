5cf70a574f02e132af95b750d158c05b4685fe88
jeaye <contact@jeaye.com>
2015-10-16 21:41:29 +0800

    Update cmake luajit check

--- cmake/check_luajit.cmake
+++ cmake/check_luajit.cmake
@@ -1,11 +1,11 @@
 message(STATUS "Checking for luajit")
 
 execute_process(
-  COMMAND vim --version | grep "luajit"
+  COMMAND "vim --version | grep luajit"
   OUTPUT_QUIET ERROR_QUIET
   RESULT_VARIABLE RESULT
 )
 
-if(RESULT EQUAL 1)
+if(RESULT EQUAL 0)
   message(FATAL_ERROR "luajit doesn't support being embedded; to use color_coded, you must install vim without luajit")
 endif()
