4ccf627952b9201836ea7094b3830d8147268893
jeaye <contact@jeaye.com>
2015-10-16 21:21:08 +0800

    Clean up some whitespace

--- cmake/generate_sources.cmake
+++ cmake/generate_sources.cmake
@@ -1,3 +1,5 @@
 message(STATUS "Generating sources")
 execute_process(
-  COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/lib/generate_sources ${CMAKE_CURRENT_SOURCE_DIR} ${LLVM_ROOT_PATH})
+  COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/lib/generate_sources
+          ${CMAKE_CURRENT_SOURCE_DIR} ${LLVM_ROOT_PATH}
+)
