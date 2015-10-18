62cd5632624e73cb91ecc63e9e591d34f403ae8f
jeaye <contact@jeaye.com>
2015-10-18 22:26:27 +0800

    Use make_absolute with a base
    This allows opening a file under a valid .color_coded file anywhere in
    the filesystem.

--- src/main.cpp
+++ src/main.cpp
@@ -66,7 +66,7 @@ namespace color_coded
 
   static int api_version(lua_State * const lua)
   {
-    std::size_t constexpr const version{ 0xc9417a4 };
+    std::size_t constexpr const version{ 0xf956488 };
     lua_pushinteger(lua, version);
     return 1;
   }
